d4b15107a8e233b2e860c7e67fe86611992b3098
“Ivan” <chrisivanortega@gmail.com>
2019-11-11 17:22:02 -0800

    adding cors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template,jsonify
 from controllers.home import HomeController
+from flask_cors import CORS
+
 hc = HomeController()
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def home():
